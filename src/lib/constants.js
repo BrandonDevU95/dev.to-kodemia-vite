9fa67674ad859db8014a0febc7b01798d447ab5c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-02 11:34:31 -0600

    feat: Add constants for server URL, access token, refresh token, and user

---
+++ src/lib/constants.js
@@ -0,0 +1,4 @@
+export const URL_SERVER = 'https://dev-to-kodemia-backend.onrender.com/api';
+export const ACCESS_TOKEN = 'accessToken';
+export const REFRESH_TOKEN = 'refreshToken';
+export const USER = 'user';
