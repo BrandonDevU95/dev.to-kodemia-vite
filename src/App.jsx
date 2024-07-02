54cdccade3488cf07edd74622150b1ed62029314
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-02 13:26:32 -0600

    refactor: Update App component layout to include left and right asides

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import MainLayout from './layouts/MainLayout';
+import LoginAsside from './components/LeftAsside/LoginAsside';
+import NavigationAsside from './components/LeftAsside/NavigationAsside';
+import PopularTags from './components/LeftAsside/PopularTags';
+import ModeratorAsside from './components/LeftAsside/ModeratorAsside';
+import FooterAsside from './components/LeftAsside/FooterAsside';
+import PostList from './components/PostList';
+import LastPostRight from './components/RightAsside/LastPostRight';
+import TrendListRight from './components/RightAsside/TrendListRight';
 
 function App() {
 	return (
 		<MainLayout>
-			<h1>Dev.to</h1>
+			<main className="py-3">
+				<div className="container">
+					<div className="row">
+						<div className="col">
+							<aside>
+								<LoginAsside />
+								<NavigationAsside />
+								<PopularTags />
+								<ModeratorAsside />
+								<FooterAsside />
+							</aside>
+						</div>
+						<div className="col-6">
+							<PostList />
+						</div>
+						<div className="col">
+							<aside className="d-grid gap-3">
+								<LastPostRight title="Discussion threads targeting the whole community" />
+								<LastPostRight title="Light, and off-topic conversation." />
+								<TrendListRight title="trending guides/resources" />
+								<TrendListRight title="recently queried" />
+							</aside>
+						</div>
+					</div>
+				</div>
+			</main>
 		</MainLayout>
 	);
 }
