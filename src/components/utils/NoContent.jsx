b936f52c192f5492ee7cee149ab0d71d90b9fb82
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-02 13:26:06 -0600

    feat: Add NoContent component for displaying a message when there is no content

---
+++ src/components/utils/NoContent.jsx
@@ -0,0 +1,9 @@
+export default function NoContent() {
+	return (
+		<div className="d-flex justify-content-center align-items-center">
+			<div className="text-center p-4">
+				<h3>Not Content</h3>
+			</div>
+		</div>
+	);
+}
