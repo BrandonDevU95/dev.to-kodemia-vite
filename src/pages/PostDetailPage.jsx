8ba57b9f1eeee6b2a27ba8cb1962aee30a718697
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-02 20:21:05 -0600

    refactor: Update PostDetailPage component to fetch and display individual post details

---
+++ src/pages/PostDetailPage.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+
+import FavDetails from '../components/Details/FavDetails';
+import LoadingSpinner from '../components/utils/Loading';
+import NoContent from '../components/utils/NoContent';
+import PostDetail from '../components/Details/PostDetail';
+import TrendingDetails from '../components/Details/TrendingDetails';
+import { getPostById } from '../api/postsAPI';
+import { getUserInfo } from '../api/usersAPI';
+import { useParams } from 'react-router-dom';
+
+export default function PostDetailPage() {
+	const { id } = useParams();
+	const [post, setPost] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchPostAndUsers = async () => {
+			try {
+				const postData = await getPostById(id);
+				const { avatar, name } = await getUserInfo(postData.author);
+
+				setPost({
+					...postData,
+					avatar,
+					name,
+				});
+			} catch (error) {
+				console.error('Error fetching post or user info:', error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchPostAndUsers();
+	}, [id]);
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (!isLoading && post.length === 0) {
+		return <NoContent />;
+	}
+
+	return (
+		<main className="py-3">
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<FavDetails
+							reactions={post.numReactions}
+							comments={post.numComments}
+						/>
+					</div>
+					<div className="col-8">
+						<div id="post-details">
+							<PostDetail post={post} />
+						</div>
+					</div>
+					<div className="col-3">
+						<TrendingDetails />
+					</div>
+				</div>
+			</div>
+		</main>
+	);
+}
