d621304fe66f5fc3521aa356310c242bd35822b5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-04 22:21:18 -0600

    refactor: Update FilterPosts component to include active tab state and filter functionality

--- src/components/FilterPosts.jsx
+++ src/components/FilterPosts.jsx
@@ -1,20 +1,53 @@
-export default function FilterPosts() {
+import clsx from 'clsx';
+import { useState } from 'react';
+
+export default function FilterPosts({ onFilter }) {
+	const [activeTab, setActiveTab] = useState(null);
+
+	const handleClick = (tab) => {
+		setActiveTab((prevTab) => (prevTab === tab ? null : tab));
+
+		if (activeTab === tab) {
+			onFilter(null); // Restablecer a todos los posts
+		} else {
+			onFilter(tab);
+		}
+	};
+
 	return (
 		<header className="mb-2">
 			<nav className="d-flex">
 				<p
 					id="relevant"
-					className="m-0 px-2_5 btn social-icon py-2 fw-lighter fs-5">
+					className={clsx(
+						'm-0 px-2_5 btn social-icon py-2 fw-lighter fs-5',
+						{
+							'fw-bold': activeTab === 'relevant',
+						}
+					)}
+					onClick={() => handleClick('relevant')}>
 					Relevant
 				</p>
 				<p
 					id="latest"
-					className="m-0 px-2_5 btn social-icon py-2 fw-lighter fs-5">
+					className={clsx(
+						'm-0 px-2_5 btn social-icon py-2 fw-lighter fs-5',
+						{
+							'fw-bold': activeTab === 'latest',
+						}
+					)}
+					onClick={() => handleClick('latest')}>
 					Latest
 				</p>
 				<p
 					id="top"
-					className="m-0 px-2_5 btn social-icon py-2 fw-lighter fs-5">
+					className={clsx(
+						'm-0 px-2_5 btn social-icon py-2 fw-lighter fs-5',
+						{
+							'fw-bold': activeTab === 'top',
+						}
+					)}
+					onClick={() => handleClick('top')}>
 					Top
 				</p>
 			</nav>
