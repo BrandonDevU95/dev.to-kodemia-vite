367e041be88da4964bf291b63c6c977fb570cd83
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-04 20:39:43 -0600

    feat: Fetch and display posts with avatars on the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,55 @@
+import { getUserData, getUserInfo } from '../api/usersAPI';
+import { useEffect, useState } from 'react';
+
 import CategoriesRight from '../components/RightAsside/CategoriesRight';
 import FooterAsside from '../components/LeftAsside/FooterAsside';
 import LastPostRight from '../components/RightAsside/LastPostRight';
+import LoadingSpinner from '../components/utils/Loading';
 import LoginAsside from '../components/LeftAsside/LoginAsside';
 import ModeratorAsside from '../components/LeftAsside/ModeratorAsside';
 import NavigationAsside from '../components/LeftAsside/NavigationAsside';
 import NewslatterAsside from '../components/LeftAsside/NewslatterAsside';
+import NoContent from '../components/utils/NoContent';
 import PopularTags from '../components/LeftAsside/PopularTags';
 import PostList from '../components/PostList';
 import TrendListRight from '../components/RightAsside/TrendListRight';
-import { getUserData } from '../api/usersAPI';
-import { useState } from 'react';
+import { getAllPost } from '../api/postsAPI';
 
 export default function HomePage() {
-	const [user, setUser] = useState(getUserData());
+	const user = getUserData();
+	const [posts, setPosts] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchPostsAndUsers = async () => {
+			try {
+				const postsData = await getAllPost();
+
+				const postsWithAvatars = await Promise.all(
+					postsData.map(async (post) => {
+						const userInfo = await getUserInfo(post.author);
+						return { ...post, avatar: userInfo.avatar };
+					})
+				);
+
+				setPosts(postsWithAvatars);
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchPostsAndUsers();
+	}, []);
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (!isLoading && posts.length === 0) {
+		return <NoContent />;
+	}
 
 	return (
 		<main className="py-3">
@@ -29,7 +66,7 @@ export default function HomePage() {
 						</aside>
 					</div>
 					<div className="col-6">
-						<PostList />
+						<PostList posts={posts} />
 					</div>
 					<div className="col">
 						<aside className="d-grid gap-3">
