e77e0a4310084cd0df6e9245eda06b4d3e728408
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-04 20:38:22 -0600

    feat: Add FilterPosts component for post filtering functionality

---
+++ src/components/FilterPosts.jsx
@@ -0,0 +1,45 @@
+export default function FilterPosts() {
+	return (
+		<nav className="d-flex justify-content-between">
+			<div>
+				<p
+					id="relevant"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5 fw-bold">
+					Relevant
+				</p>
+				<p
+					id="latest"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Latest
+				</p>
+				<p
+					id="top"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Top
+				</p>
+			</div>
+			<div>
+				<p
+					id="relevant"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Week
+				</p>
+				<p
+					id="latest"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Month
+				</p>
+				<p
+					id="top"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Year
+				</p>
+				<p
+					id="top"
+					className="m-0 px-2_5 btn social-icon py-2 fw-normal fs-5_5">
+					Infinity
+				</p>
+			</div>
+		</nav>
+	);
+}
