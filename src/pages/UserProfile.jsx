af9b25a941aa114ac8b3fdfa742e4e474de1d79d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-05 19:28:25 -0600

    feat: Add UserProfile page component

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,11 @@
+import NoContent from '../components/utils/NoContent';
+
+export default function UserProfile() {
+	return (
+		<main className="py-3">
+			<div className="container">
+				<NoContent />
+			</div>
+		</main>
+	);
+}
